133d72f346a4de00f4809525fab32785441b411e
Robinson Souza <Robinsond76@gmail.com>
2020-04-15 17:01:35 -0400

    Connected the app to the database on MongoDB Atlas

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config'); //we bring this in as we need access to that global variable
+const db = config.get('mongoURI'); //because we brought in config, we have access to the mongoURI
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Mongodb Connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); //Will exit with failure
+  }
+};
+
+module.exports = connectDB;
