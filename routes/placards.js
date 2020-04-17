f079e13aca620ad0c05340f58df7e16ec6fa806e
Robinson Souza <Robinsond76@gmail.com>
2020-04-17 10:47:15 -0400

    Update and Delete Placard Route, + get all user placards route

--- routes/placards.js
+++ routes/placards.js
@@ -22,6 +22,22 @@ router.get('/', async (req, res) => {
   }
 });
 
+//@route    GET api/placards/user
+//@desc     Get all user placards
+//@access   Private
+router.get('/user', auth, async (req, res) => {
+  try {
+    const placards = await Placard.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+
+    res.json(placards);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 //@route    POST api/placards
 //@desc     Add new placard
 //@access   Private
@@ -60,15 +76,62 @@ router.post(
 //@route    PUT api/placards/:id
 //@desc     Update placard
 //@access   Private
-router.put('/:id', (req, res) => {
-  res.send('Update placard');
+router.put('/:id', auth, async (req, res) => {
+  const { title, desc, img, email, phone, type } = req.body;
+
+  //Build a placard object
+  const placardFields = {};
+  if (title) placardFields.title = title;
+  if (desc) placardFields.desc = desc;
+  if (img) placardFields.img = img;
+  if (email) placardFields.email = email;
+  if (phone) placardFields.phone = phone;
+  if (type) placardFields.type = type;
+
+  try {
+    let placard = await Placard.findById(req.params.id);
+
+    if (!placard) return res.status(404).json({ msg: 'Placard not found' });
+
+    //Make sure user owns placard
+    if (placard.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not Authorized' });
+    }
+
+    placard = await Placard.findByIdAndUpdate(
+      req.params.id,
+      { $set: placardFields },
+      { new: true }
+    );
+
+    res.json(placard);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 //@route    DELETE api/placards/:id
 //@desc     Delete placard
 //@access   Private
-router.delete('/:id', (req, res) => {
-  res.send('Delete placard');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let placard = await Placard.findById(req.params.id);
+
+    if (!placard) return res.status(404).json({ msg: 'Placard not found' });
+
+    //Make sure user owns placard
+    if (placard.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not Authorized' });
+    }
+
+    await Placard.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: 'Placard Removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
