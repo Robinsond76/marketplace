6933850d41642912df7c0e52ea9b3d8cf03053e0
Robinson Souza <Robinsond76@gmail.com>
2020-04-21 10:00:44 -0400

    installed materialize, created navbar and implemented it

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
+import Navbar from './components/layout/Navbar.js';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 const App = () => {
-  return <div className='App'>My App</div>;
+  useEffect(() => {
+    //Import Materialize JS
+    M.AutoInit();
+  });
+
+  return (
+    <Fragment>
+      <Navbar />
+      <div className='container'>My App</div>;
+    </Fragment>
+  );
 };
 
 export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react';
+
+const Navbar = () => {
+  return (
+    <Fragment>
+      <nav className='teal darken-4'>
+        <div class='nav-wrapper'>
+          <a href='#!' class='brand-logo'>
+            Marketplace
+          </a>
+          <a href='#!' data-target='mobile-menu' class='sidenav-trigger'>
+            <i class='material-icons'>menu</i>
+          </a>
+          <ul class='right hide-on-med-and-down'>
+            <li>
+              <a href='#!'>About</a>
+            </li>
+            <li>
+              <a href='#!'>Login</a>
+            </li>
+            <li>
+              <a href='#!'>Register</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <ul class='sidenav' id='mobile-menu'>
+        <li>
+          <a href='#!'>About</a>
+        </li>
+        <li>
+          <a href='#!'>Login</a>
+        </li>
+        <li>
+          <a href='#!'>Register</a>
+        </li>
+      </ul>
+    </Fragment>
+  );
+};
+
+export default Navbar;
